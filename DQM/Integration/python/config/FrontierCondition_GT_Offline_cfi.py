6534b345d2fc99a37dba319623a32c8695f9f10e
Emma Yeager <emmayeager0@gmail.com>
2016-10-06 14:04:15 +0200

    commit

---
+++ DQM/Integration/python/config/FrontierCondition_GT_Offline_cfi.py
@@ -0,0 +1,8 @@
+#added by Emma
+
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import *
+from Configuration.AlCa.GlobalTag import GlobalTag as gtCustomise
+
+GlobalTag = gtCustomise(GlobalTag, 'auto:run2_data', '')
