4e2e2bb85ae6ee4923266333e78b3b4c5be858ed
Emma Yeager <emmayeager0@gmail.com>
2016-12-15 17:35:10 +0100

    root file with contentSigma up to date

--- DQM/Integration/python/config/fileinputsource_cfi.py
+++ DQM/Integration/python/config/fileinputsource_cfi.py
@@ -14,11 +14,26 @@ source = cms.Source(
 	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/0208FF04-EB91-E611-965C-02163E01389B.root",
 	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/0212579C-3C92-E611-B0ED-02163E014348.root",
 	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/029C87AA-3C92-E611-956C-02163E014380.root",
+	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/02DA1D23-4092-E611-AA04-FA163E35234A.root",
+	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/02E9BAA7-3C92-E611-95C6-02163E01198B.root",
+	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/04213E9F-EC91-E611-BBE3-02163E013425.root",
+	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/0432FCA5-EE91-E611-8E1E-02163E011970.root",
+	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/043FB294-3C92-E611-A4CA-FA163E2C38F6.root",
+	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/0453FA53-EA91-E611-981F-02163E0140EB.root",
+	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/047A8447-4092-E611-B151-02163E0118E6.root",
+	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/067F96A8-EE91-E611-9FC2-FA163EB6B8B4.root",
+	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/06B3158E-3C92-E611-84E2-FA163EA0D418.root",
+	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/06BCCC0B-EB91-E611-BE14-02163E0118A5.root",
+	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/06C9C586-3C92-E611-81A5-FA163ECD93DC.root",
+	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/083B8F9E-3C92-E611-A9A3-02163E0146CA.root",
+	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/086F7994-3C92-E611-8014-FA163E073540.root",
+	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/0870D7A6-EE91-E611-B822-02163E011AB4.root",
+	"/store/data/Run2016H/ZeroBiasIsolatedBunch0/RAW/v1/000/283/171/00000/08FE108A-EE91-E611-A031-FA163EE1765D.root",
     )
 )
 
 maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(500)
+    input = cms.untracked.int32(100000)
 )
 
 # Parameters for runType
